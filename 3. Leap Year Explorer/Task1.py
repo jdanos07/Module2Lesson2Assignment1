217ff5d22948d72f351b85dbc20453b6a75d833b
jdanos07 <jdanos07@gmail.com>
2024-09-10 10:40:02 -0400

    adding part 3

---
+++ 3. Leap Year Explorer/Task1.py
@@ -0,0 +1,16 @@
+# Leap Year Checker Write a Python program that prompts the user to input a year. 
+# The program should determine if the given year is a leap year or not 
+# and then display an appropriate message. Please note that this is the
+# definition of a leap year: Every year that is exactly divisible by four 
+# is a leap year, except for years that are exactly divisible by 100, but
+# these centurial years are leap years if they are exactly divisible by 400.
+
+print("Enter year to determine if it is a leap year:")
+year = int(input())
+
+if year%4 == 0 and year%400 == 0:
+    print("It's a leap year")
+elif year%4 == 0:
+    print("It's a leap year")
+else:
+    print("it's another year, but not a leap year")
