0564dadfa00c5847de1bd658faec65e3d9e14bf7
jdanos07 <jdanos07@gmail.com>
2024-09-10 17:57:51 -0400

    system indicated modified

--- 2. The Greatest Showdown/Task1.py
+++ 2. The Greatest Showdown/Task1.py
@@ -11,6 +11,6 @@ number3 = int(input())
 if number1 > number2 and number1 > number3:
     print(number1, "is the largest of the group")
 elif number2 > number1 and number2 > number3:
-    print(number2, "is the largest of the bunch!")
+    print(number2, "is the biggest of the bunch!")
 else:
     print(number3, "takes the cake")
